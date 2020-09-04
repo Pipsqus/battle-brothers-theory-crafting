fe208c5c61ea57f1804e2871ea90ea11b0af9eea
Pipsqus <andreaforconi@protonmail.com>
2020-09-04 18:38:09 +0200

    First commit

---
+++ melee.js
@@ -0,0 +1,27 @@
+function SwordsView() {
+  document.getElementById("SwordsView").scrollIntoView()
+}
+function DaggersView() {
+  document.getElementById("DaggersView").scrollIntoView() 
+}
+function MacesView() {
+  document.getElementById("MacesView").scrollIntoView()
+}
+function HammersView() {
+  document.getElementById("HammersView").scrollIntoView()
+}
+function SpearsView() {
+  document.getElementById("SpearsView").scrollIntoView()
+}
+function AxesView() {
+  document.getElementById("AxesView").scrollIntoView()
+}
+function FlailsView() {
+  document.getElementById("FlailsView").scrollIntoView()
+}
+function CleaversView() {
+  document.getElementById("CleaversView").scrollIntoView()
+}
+function PolearmsView() {
+  document.getElementById("PolearmsView").scrollIntoView()
+}
